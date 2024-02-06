6a4ec29e849b6641532f20863a1ea2e8b13e3c06
bzhang102 <yuxiangbrandonzhang@gmail.com>
2024-02-06 11:12:57 -0800

    HW 2 Complete

--- homework_2/homework_2/eval.cpp
+++ homework_2/homework_2/eval.cpp
@@ -7,12 +7,15 @@
 
 using namespace std;
 
-int toPostfix(string infix, string& postfix);
+int toPostfix(string infix, string& postfix, int& i, bool& inParen);
 int getPrecedence(char c);
 int operate(int x, int y, char operation);
 
 int evaluate(string infix, const Map& values, string& postfix, int& result) {
-    if(toPostfix(infix, postfix) == 1) {
+    int i = 0;
+    bool inParen = false;
+    postfix = "";
+    if(toPostfix(infix, postfix, i, inParen) == 1) {
         return 1;
     }
 
@@ -46,57 +49,49 @@ int evaluate(string infix, const Map& values, string& postfix, int& result) {
     return 0;
 }
 
-int toPostfix(string infix, string& postfix) {
-    postfix = "";
+int toPostfix(string infix, string& postfix, int& i, bool& inParen) {
     stack<char> oStack;
 
     int counter = 0;
-    char cur;
-    for(int i = 0; i < infix.size(); i++) {
-        cur = infix[i];
-
-        if(islower(cur)) {
+    bool openParen = false;
+    for(; i < infix.size(); i++) {
+        if(islower(infix[i])) {
             counter++;
-            postfix += cur;
+            postfix += infix[i];
 
-        } else if(cur == '(') {
-            oStack.push(cur);
-
-        } else if(cur == ')') {
-            while(oStack.top() != '(') {
-                postfix += oStack.top();
-                oStack.pop();
+        } else if(infix[i] == '(') {
+            i++;
+            openParen = true;
+            if(toPostfix(infix, postfix, i, openParen) == 1) {
+                return 1;
             }
-            oStack.pop();
-
-        } else if(cur == '*' || cur == '/' || cur == '+' || cur == '-') {
+            counter++;
+        } else if(infix[i] == ')') {
+            if(inParen == false) {
+                return 1;
+            }
+            inParen = false;
+            break;
+        } else if(infix[i] == '*' || infix[i] == '/' || infix[i] == '+' || infix[i] == '-') {
             counter--;
-            while(!oStack.empty() && oStack.top() != '(' && getPrecedence(cur) <= getPrecedence(oStack.top())) {
-                if(oStack.top() == '(') {
-                    return 1;
-                }
+            while(!oStack.empty() && oStack.top() != '(' && getPrecedence(infix[i]) <= getPrecedence(oStack.top())) {
                 postfix += oStack.top();
                 oStack.pop();
             }
-            oStack.push(cur);
+            oStack.push(infix[i]);
 
-        } else if(cur != ' ') {
+        } else if(infix[i] != ' ') {
             return 1;
         }
 
-        if(counter < 0 || counter > 1) {
-            return 1;
-        }
+        if(counter < 0 || counter > 1) { return 1;}
     }
     while(!oStack.empty()) {
-        if(oStack.top() == '(') {
-            return 1;
-        }
         postfix += oStack.top();
         oStack.pop();
     }
 
-    return (counter == 1) ? 0 : 1;
+    return (counter == 1 && !inParen) ? 0 : 1;
 }
 
 int getPrecedence(char c) {
@@ -115,41 +110,4 @@ int operate(int x, int y, char operation) {
             return x / y;
     }
     return 0;
-}
-
-int main() {
-        char vars[] = { 'a', 'e', 'i', 'o', 'u', 'y', '#' };
-        int  vals[] = {  3,  -9,   6,   2,   4,   1  };
-        Map m;
-        for (int k = 0; vars[k] != '#'; k++)
-            m.insert(vars[k], vals[k]);
-        string pf;
-        int answer;
-        assert(evaluate("a+ e", m, pf, answer) == 0  &&
-               pf == "ae+"  &&  answer == -6);
-        answer = 999;
-        assert(evaluate("", m, pf, answer) == 1  &&  answer == 999);
-        assert(evaluate("a+", m, pf, answer) == 1  &&  answer == 999);
-        assert(evaluate("a i", m, pf, answer) == 1  &&  answer == 999);
-        assert(evaluate("ai", m, pf, answer) == 1  &&  answer == 999);
-        assert(evaluate("()", m, pf, answer) == 1  &&  answer == 999);
-//        assert(evaluate("()o", m, pf, answer) == 1  &&  answer == 999);
-        assert(evaluate("y(o+u)", m, pf, answer) == 1  &&  answer == 999);
-//        assert(evaluate("y(*o)", m, pf, answer) == 1  &&  answer == 999);
-        assert(evaluate("a+E", m, pf, answer) == 1  &&  answer == 999);
-        assert(evaluate("(a+(i-o)", m, pf, answer) == 1  &&  answer == 999);
-    //    // unary operators not allowed:
-        assert(evaluate("-a", m, pf, answer) == 1  &&  answer == 999);
-        assert(evaluate("a*b", m, pf, answer) == 2  &&
-               pf == "ab*"  &&  answer == 999);
-        assert(evaluate("y +o *(   a-u)  ", m, pf, answer) == 0  &&
-               pf == "yoau-*+"  &&  answer == -1);
-        answer = 999;
-        assert(evaluate("o/(y-y)", m, pf, answer) == 3  &&
-               pf == "oyy-/"  &&  answer == 999);
-        assert(evaluate(" a  ", m, pf, answer) == 0  &&
-               pf == "a"  &&  answer == 3);
-        assert(evaluate("((a))", m, pf, answer) == 0  &&
-               pf == "a"  &&  answer == 3);
-        cout << "Passed all tests" << endl;
 }
--- homework_2/homework_2/mazestack.cpp
+++ homework_2/homework_2/mazestack.cpp
@@ -1,47 +1,47 @@
-//#include <string>
-//#include <stack>
-//
-//using namespace std;
-//
-//class Coord
-//{
-//public:
-//    Coord(int r, int c) : m_r(r), m_c(c) {}
-//    int r() const { return m_r; }
-//    int c() const { return m_c; }
-//private:
-//    int m_r;
-//    int m_c;
-//};
-//
-//
-//bool pathExists(string maze[], int nRows, int nCols, int sr, int sc, int er, int ec) {
-//    stack<Coord> coordStack;
-//    coordStack.push(Coord(sr,sc));
-//    maze[sr][sc] = 'X';
-//
-//    while(!coordStack.empty()) {
-//        Coord cur = coordStack.top();
-//        maze[cur.r()][cur.c()] = 'X';
-//        coordStack.pop();
-//
-//        if(cur.r() == er && cur.c() == ec) {
-//            return true;
-//        }
-//
-//        if(maze[cur.r() - 1][cur.c()] == '.') {
-//            coordStack.push(Coord(cur.r() - 1, cur.c()));
-//        }
-//        if(maze[cur.r()][cur.c() - 1] == '.') {
-//            coordStack.push(Coord(cur.r(), cur.c() - 1));
-//        }
-//        if(maze[cur.r() + 1][cur.c()] == '.') {
-//            coordStack.push(Coord(cur.r() + 1, cur.c()));
-//        }
-//        if(maze[cur.r()][cur.c() + 1] == '.') {
-//            coordStack.push(Coord(cur.r(), cur.c() + 1));
-//        }
-//    }
-//
-//    return false;
-//}
+#include <string>
+#include <stack>
+
+using namespace std;
+
+class Coord
+{
+public:
+    Coord(int r, int c) : m_r(r), m_c(c) {}
+    int r() const { return m_r; }
+    int c() const { return m_c; }
+private:
+    int m_r;
+    int m_c;
+};
+
+
+bool pathExists(string maze[], int nRows, int nCols, int sr, int sc, int er, int ec) {
+    stack<Coord> coordStack;
+    coordStack.push(Coord(sr,sc));
+    maze[sr][sc] = 'X';
+
+    while(!coordStack.empty()) {
+        Coord cur = coordStack.top();
+        maze[cur.r()][cur.c()] = 'X';
+        coordStack.pop();
+
+        if(cur.r() == er && cur.c() == ec) {
+            return true;
+        }
+
+        if(maze[cur.r() - 1][cur.c()] == '.') {
+            coordStack.push(Coord(cur.r() - 1, cur.c()));
+        }
+        if(maze[cur.r()][cur.c() - 1] == '.') {
+            coordStack.push(Coord(cur.r(), cur.c() - 1));
+        }
+        if(maze[cur.r() + 1][cur.c()] == '.') {
+            coordStack.push(Coord(cur.r() + 1, cur.c()));
+        }
+        if(maze[cur.r()][cur.c() + 1] == '.') {
+            coordStack.push(Coord(cur.r(), cur.c() + 1));
+        }
+    }
+
+    return false;
+}
