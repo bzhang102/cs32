016ec884453208ec7ef370e8e786431d4e496ba3
bzhang102 <yuxiangbrandonzhang@gmail.com>
2024-02-13 17:05:20 -0800

    Submission Ready 2.0

--- homework_3/homework_3/maze.cpp
+++ homework_3/homework_3/maze.cpp
@@ -1,7 +1,3 @@
-#include <string>
-
-using namespace std;
-
 class Coord {
 public:
     Coord(int r, int c) : m_r(r), m_c(c) {}
@@ -12,15 +8,11 @@ private:
     int m_c;
 };
 
-bool dfs(string maze[], int r, int c, int er, int ec) {
-    maze[r][c] = 'X';
-    if(r == er && c == ec) return true;
-    return (maze[r][c + 1] == '.' && dfs(maze, r, c + 1, er, ec)) ||
-            (maze[r + 1][c] == '.' && dfs(maze, r + 1, c, er ,ec)) ||
-            (maze[r][c - 1] == '.' && dfs(maze, r, c - 1, er ,ec)) ||
-            (maze[r - 1][c] == '.' && dfs(maze, r - 1, c, er ,ec));
-}
-
 bool pathExists(string maze[], int nRows, int nCols, int sr, int sc, int er, int ec) {
-    return dfs(maze, sr, sc, er, ec);
+    maze[sr][sc] = 'X';
+    if(sr == er && sc == ec) return true;
+    return (maze[sr][sc + 1] == '.' && pathExists(maze, nRows, nCols, sr, sc + 1, er, ec)) ||
+            (maze[sr + 1][sc] == '.' && pathExists(maze, nRows, nCols, sr + 1, sc, er ,ec)) ||
+            (maze[sr][sc - 1] == '.' && pathExists(maze, nRows, nCols, sr, sc - 1, er ,ec)) ||
+            (maze[sr - 1][sc] == '.' && pathExists(maze, nRows, nCols, sr - 1, sc, er ,ec));
 }
