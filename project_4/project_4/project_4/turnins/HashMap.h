605f9bf58b0df5e8b33cbac38630a43f0ff15c36
bzhang102 <yuxiangbrandonzhang@gmail.com>
2024-03-10 15:42:50 -0700

    HashMap Complete (for real i think)

--- project_4/project_4/project_4/turnins/HashMap.h
+++ project_4/project_4/project_4/turnins/HashMap.h
@@ -13,12 +13,15 @@ public:
     int size() const { return m_size; }
 
     void insert(const std::string& key, const T& value);
-    T& operator[](const std::string& key) { return find(key); }
+    T& operator[](const std::string& key);
 
     // Const
     const T* find(const std::string& key) const;
     // Nonconst
-    T* find(const std::string& key) { return const_cast<T*>(this->find(key)); }
+    T* find(const std::string& key) {
+        const auto& hm = *this;
+        return const_cast<T*>(hm.find(key));
+    }
 
     // No copying or assigning
     HashMap(const HashMap&) = delete;
@@ -64,21 +67,39 @@ HashMap<T>::~HashMap() {
 // insert()
 template <typename T>
 void HashMap<T>::insert(const std::string& key, const T& value) {
-    m_size++;
-    // if oversize, rehash
-    if(m_size >= m_maxLoad) { rehash(); }
-
+    // check for duplicate keys
+    for(auto item : m_hashTable[hash(key)]) {
+        if(item->m_key == key) {
+            item->m_value = value;
+            return;
+        }
+    }
     // add to correct bucket
     m_hashTable[hash(key)].push_back(new Item(key, value));
+    m_size++;
+    // if oversize, rehash
+    if(m_size * 1.0 / m_numBuckets >= m_maxLoad) { rehash(); }
 }
+
+// operator[]()
+template <typename T>
+T& HashMap<T>::operator[](const std::string& key) { 
+    T* valuePtr = find(key);
+    // if exists return value
+    if(valuePtr != nullptr) return *valuePtr;
+    // if not insert and return new default value
+    insert(key, T());
+    return *find(key);
+}
+
 
 // find()
 template <typename T>
 const T* HashMap<T>::find(const std::string& key) const {
     // iterate through corresponding bucket
-    for(auto obj : *m_hashTable[hash(key)]) {
+    for(auto obj : m_hashTable[hash(key)]) {
         if(obj->m_key == key) {
-            return &obj->m_val;
+            return &obj->m_value;
         }
     }
     // not found
@@ -97,7 +118,7 @@ void HashMap<T>::rehash() {
     // move all objects into new vector
     for(auto bucket : m_hashTable) {
         for(auto obj : bucket) {
-            newTable[hash(obj->m_key)].pushback(obj);
+            newTable[hash(obj->m_key)].push_back(obj);
         }
     }
     // reassign map's vector
