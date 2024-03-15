72c3edf2c8b2f1dc804d2294ac6c70696f1bf3a4
bzhang102 <yuxiangbrandonzhang@gmail.com>
2024-03-14 22:54:14 -0700

    project 4 turn in

--- project_4/project_4/project_4/turnins/tour_generator.cpp
+++ project_4/project_4/project_4/turnins/tour_generator.cpp
@@ -3,6 +3,7 @@
 #include "geotools.h"
 
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
@@ -23,39 +24,44 @@ vector<TourCommand> TourGenerator::generate_tour(const Stops& stops) const {
         if(i < stops.size() - 1) {
             // get start point
             GeoPoint start;
-            m_db.get_poi_location(nextPOI, start);
+            if(!m_db.get_poi_location(nextPOI, start)) {
+                stops.get_poi_data(i + 1, nextPOI, talkingPoints);
+                continue;
+            }
             // find next poi
             stops.get_poi_data(i + 1, nextPOI, talkingPoints);
             // get endpoint
             GeoPoint end;
-            m_db.get_poi_location(nextPOI, end);
+            if(!m_db.get_poi_location(nextPOI, end)) continue;
             // get route
             vector<GeoPoint> route = m_router.route(start, end);
-            for(int i = 0; i < route.size() - 1; i++) {
-                GeoPoint& pt1 = route[i];
-                GeoPoint& pt2 = route[i + 1];
+            if(route.size() != 0) {
+                for(int i = 0; i < route.size() - 1; i++) {
+                    GeoPoint& pt1 = route[i];
+                    GeoPoint& pt2 = route[i + 1];
 
-                // push proceed command
-                TourCommand proceed;
-                proceed.init_proceed(getDirection(pt1, pt2), m_db.get_street_name(pt1, pt2), distance_earth_miles(pt1, pt2), pt1, pt2);
-                instructions.push_back(proceed);
+                    // push proceed command
+                    TourCommand proceed;
+                    proceed.init_proceed(getDirection(pt1, pt2), m_db.get_street_name(pt1, pt2), distance_earth_miles(pt1, pt2), pt1, pt2);
+                    instructions.push_back(proceed);
 
-                // if there are two points left
-                if(i < route.size() - 2) {
-                    GeoPoint& pt3 = route[i + 2];
-                    // if street change
-                    if(m_db.get_street_name(pt1, pt2) != m_db.get_street_name(pt2, pt3)) {
-                        double angle = angle_of_turn(pt1, pt2, pt3);
-                        // left turn
-                        if(angle >= 1 && angle < 180) {
-                            TourCommand turn;
-                            turn.init_turn("left", m_db.get_street_name(pt2, pt3));
-                            instructions.push_back(turn);
-                        // right turn
-                        } else if(angle >= 180 && angle <= 359) {
-                            TourCommand turn;
-                            turn.init_turn("right", m_db.get_street_name(pt2, pt3));
-                            instructions.push_back(turn);
+                    // if there are two points left
+                    if(i < route.size() - 2) {
+                        GeoPoint& pt3 = route[i + 2];
+                        // if street change
+                        if(m_db.get_street_name(pt1, pt2) != m_db.get_street_name(pt2, pt3)) {
+                            double angle = angle_of_turn(pt1, pt2, pt3);
+                            // left turn
+                            if(angle >= 1 && angle < 180) {
+                                TourCommand turn;
+                                turn.init_turn("left", m_db.get_street_name(pt2, pt3));
+                                instructions.push_back(turn);
+                                // right turn
+                            } else if(angle >= 180 && angle <= 359) {
+                                TourCommand turn;
+                                turn.init_turn("right", m_db.get_street_name(pt2, pt3));
+                                instructions.push_back(turn);
+                            }
                         }
                     }
                 }
